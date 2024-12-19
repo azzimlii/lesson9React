409b133543936db8d65c59c35467756a4abafc92
azzimlii <ezimliinci@gmail.com>
2024-12-19 21:36:39 +0400

    rename folders

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
 import Nav from './components/Nav'
 import Navbar from './components/Navbar'
-import About from './pages/firstRouting/about/About'
-import Contact from './pages/firstRouting/contact/Contact'
-import Work from './pages/firstRouting/workExamples/Work'
+import About from './pages/portfolio/about/About'
+import Contact from './pages/portfolio/contact/Contact'
+import Work from './pages/portfolio/workExamples/Work'
 import Clock from './pages/secondRouting/clock/Clock'
 import StopWatch from './pages/secondRouting/stopWatch/StopWatch'
 import Timer from './pages/secondRouting/timer/Timer'
 import AppProject from './tasks/additionalproject/AppProject'
-import AppTask1 from './tasks/task1/AppTask1'
-import AppTask2 from './tasks/task2/AppTask2'
-import AppTask3 from './tasks/task3/AppTask3'
+import AppTask1 from './tasks/customButton/AppTask1'
+import AppTask2 from './tasks/employeeList/AppTask2'
+import AppTask3 from './tasks/cardForm/AppTask3'
 import AppTask4 from './tasks/task4/AppTask4'
 import AppTask5 from './tasks/task5/AppTask5'
 
@@ -27,7 +27,7 @@ function App() {
 
       {/* <AppTask1/> */}
       {/* <AppTask2/> */}
-      {/* <AppTask3/> */}
+      {/* <AppTask3/>  index.html'da css'i acaq zehmet olmasa */}
       {/* <AppTask4/> */}
       {/* <AppProject/> */}
       {/* <AppTask5/> */}
